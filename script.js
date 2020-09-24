8fa8a24b9985eef0142ecd04d303253c56bbc6ad
Nazariy995 <nazariy1995@gmail.com>
2020-09-24 14:09:41 -0700

    ADD: Sep 24 call  - API

--- script.js
+++ script.js
@@ -2,16 +2,34 @@
 
 
 window.onload = () =>  {
-    addMovies();
+    fetchMovies();
+}
+
+function fetchMovies(){
+    fetch("https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213")
+    .then((response)=>{
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("something went wrong");
+        }
+    })
+    .then((data)=>{
+        addMovies(data);
+    })
+    .catch((error_data)=>{
+        console.log(error_data);
+    })
 }
 
 // Add movies to the front end
-function addMovies(){
-
+function addMovies(movies){
     // Add img element to original__movies element
     var moviesEl = document.querySelector('.original__movies');
-    console.log(moviesEl);
-    for(var i = 0; i < 5;  i++){
-        moviesEl.innerHTML += '<img src="https://image.tmdb.org/t/p/original//scZlQQYnDVlnpxFTxaIv2g0BWnL.jpg"></img>';
+    for(var movie of movies.results){
+        var image = `
+            <img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></img>
+        `
+        moviesEl.innerHTML += image;
     }
 }
