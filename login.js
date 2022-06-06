526e9c586f818f5a85224197e1e1b6267f9465f9
David Rakosi <61084415+davidrakosi@users.noreply.github.com>
2022-06-06 07:34:35 -0700

    ✅

--- login.js
+++ login.js
@@ -1,43 +1,43 @@
-let loginForm = document.getElementById("loginForm");
-let apiUrl =  "http://localhost:3000";
-if(location.href.indexOf("netlify") >= 0 ){
-    apiUrl = "https://netflix-cp.herokuapp.com";
+let loginForm = document.getElementById('loginForm')
+let apiUrl = 'http://localhost:3000'
+if (location.href.indexOf('netlify') >= 0) {
+  apiUrl = 'https://netflix-cp.herokuapp.com'
 }
 
-const queryString = location.search;
-const urlParams = new URLSearchParams(queryString);
-const existingEmail = urlParams.get('existingEmail');
+const queryString = location.search
+const urlParams = new URLSearchParams(queryString)
+const existingEmail = urlParams.get('existingEmail')
 const registered = urlParams.get('registered')
-if(existingEmail){
-    loginForm.email.value = existingEmail
+if (existingEmail) {
+  loginForm.email.value = existingEmail
 }
-if(registered){
-    document.querySelector('.registered-alert').style.display = "block";
+if (registered) {
+  document.querySelector('.registered-alert').style.display = 'block'
 }
 
-loginForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    console.log(loginForm);
-    let payload = {
-        email: loginForm.email.value,
-        password: loginForm.password.value
-    }
-    fetch(apiUrl + "/login", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-    })
-    .then((response)=>{
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("something went wrong");
-        }
+loginForm.addEventListener('submit', e => {
+  e.preventDefault()
+  console.log(loginForm)
+  let payload = {
+    email: loginForm.email.value,
+    password: loginForm.password.value,
+  }
+  fetch(apiUrl + '/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('something went wrong')
+      }
     }) // returns a promise already
-    .then((response)=>{
-        localStorage.setItem('token', response.token)
-        location.href = "/";
+    .then(response => {
+      localStorage.setItem('token', response.token)
+      location.href = '/'
     })
 })
--- session.js
+++ session.js
@@ -1,26 +1,28 @@
-function checkIfLoggedIn(){
-    const currentToken = localStorage.getItem('token');
-    if(currentToken){
-        if(location.href.includes("/login.html")
-            || location.href.includes("/register.html")){
-            location.href = "/";
-        }
-    } else {
-        // If I am currently not logged in
-        // And trying to acceess a unauthorized page
-        // (Trying to access all pages besides login)
-        if(!location.href.includes("/login.html")
-            && !location.href.includes("/register.html")) {
-            location.href = '/login.html';
-        }
+function checkIfLoggedIn() {
+  const currentToken = localStorage.getItem('token')
+  if (currentToken) {
+    if (
+      location.href.includes('/login.html') ||
+      location.href.includes('/register.html')
+    ) {
+      location.href = '/'
     }
+  } else {
+    // If I am currently not logged in
+    // And trying to acceess a unauthorized page
+    // (Trying to access all pages besides login)
+    if (
+      !location.href.includes('/login.html') &&
+      !location.href.includes('/register.html')
+    ) {
+      location.href = '/login.html'
+    }
+  }
 }
 
-function logOut(){
-    localStorage.removeItem('token');
-    location.href = '/login.html';
+function logOut() {
+  localStorage.removeItem('token')
+  location.href = '/login.html'
 }
 
-checkIfLoggedIn();
-
-
+checkIfLoggedIn()
